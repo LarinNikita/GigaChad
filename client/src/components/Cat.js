73ced7ec75d944bc7c11f298667fa040b0696f0b
LarinNikita <larin_nikita_m@mail.ru>
2023-07-27 17:03:45 +0700

    sidebar and switch theme

---
+++ client/src/components/Cat.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Avatar } from '@mui/material'
+
+const Cat = () => {
+    return (
+        <Avatar src="https://img.freepik.com/free-photo/grey-kitty-with-monochrome-wall-her_23-2148955113.jpg?size=626&ext=jpg&ga=GA1.2.1650180769.1690447037&semt=sph" />
+    )
+}
+
+export default Cat
