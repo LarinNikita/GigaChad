75931c318f07455a1c5ac621def307dc1b2f9ea2
LarinNikita <80145142+LarinNikita@users.noreply.github.com>
2023-08-01 20:22:07 +0700

    Revert "Backend"

--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const authController = require("../controllers/authController");
-const userController = require("../controllers/userController");
-
-router.patch("/update-me", authController.protect, userController.updateMe);
-
-module.exports = router;
